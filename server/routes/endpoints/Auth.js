70acaeebb2018756ef4cc06a65d4a043665179b8
Meedo365 <nuhuahmed365@gmail.com>
2024-08-11 12:09:22 +0100

    Create Auth.js

---
+++ server/routes/endpoints/Auth.js
@@ -0,0 +1,25 @@
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  login,
+  getUserByToken,
+  logout,
+  changeUserPasswordById,
+} = require("../../controllers/auth-controller");
+const { auth } = require("../../middlewares/middleware");
+let routes = (app) => {
+  app.post("/register", createUser);
+  app.get("/users", getAllUsers);
+  app.get("/users/:id", getUserById);
+  app.get("/token", auth, getUserByToken);
+  app.put("/user/:id", updateUserById);
+  app.put("/password/user/", changeUserPasswordById);
+  app.delete("/user/:id", deleteUserById);
+  app.post("/login", login);
+  app.post("/logout", auth, logout);
+};
+
+module.exports = routes;
