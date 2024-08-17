195bcbacfeb1ff5da4679737f0f172d38bffa4cb
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2024-08-17 22:07:49 +0100

    fix button

--- admin/src/pages/eventDetails/index.tsx
+++ admin/src/pages/eventDetails/index.tsx
@@ -24,8 +24,8 @@ const EventDetails: React.FC = () => {
     description: string;
     organizer: string;
     id: number;
-    invoiceStatus: string;
     countId: number;
+    invoiceStatus: string;
   };
 
   if (!event) {
@@ -53,7 +53,7 @@ const EventDetails: React.FC = () => {
   const { mutate: mutateGenerateInvoice, isLoading: loadingGenerateInvoice } =
     useGenerateInvoice();
 
-  const handeEActionvent = async (type: string) => {
+  const handelActionEvent = async (type: string) => {
     if (type === "approve") {
       mutateApproval(id);
     } else if (type === "decline") {
@@ -61,140 +61,139 @@ const EventDetails: React.FC = () => {
     } else {
       mutateGenerateInvoice(countId);
     }
+  };
 
-    console.log("events", event);
-    return (
-      <>
-        {loadingApproval ||
-          loadingGenerateInvoice ||
-          (loadingDecline && <Loader />)}
+  return (
+    <>
+      {loadingApproval ||
+        loadingDecline ||
+        (loadingGenerateInvoice && <Loader />)}
 
-        <div className="px-5 py-10 bg-gray-100">
-          <div className="flex items-center justify-start mb-5">
-            <Button
-              variant="outlined"
-              color="success"
-              startIcon={<IoArrowBackCircleOutline />}
-              onClick={() => window.history.back()}
-            >
-              Go Back
-            </Button>
+      <div className="px-5 py-10 bg-gray-100">
+        <div className="flex items-center justify-start mb-5">
+          <Button
+            variant="outlined"
+            color="success"
+            startIcon={<IoArrowBackCircleOutline />}
+            onClick={() => window.history.back()}
+          >
+            Go Back
+          </Button>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="h-[200px] sm:h-[300px] md:h-[400px] w-full">
+            <img
+              src={image}
+              alt={title}
+              className="w-full h-full object-cover rounded-3xl shadow-lg"
+            />
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <div className="h-[200px] sm:h-[300px] md:h-[400px] w-full">
-              <img
-                src={image}
-                alt={title}
-                className="w-full h-full object-cover rounded-3xl shadow-lg"
-              />
+        </div>
+        <div className="relative  border-green-200 pb-6 sm:pb-8 md:pb-10">
+          <div className="mt-6 sm:mt-8 md:mt-10 grid gap-4 sm:gap-6 md:gap-8">
+            <div>
+              {status === "approved" ? (
+                <Chip
+                  label={status}
+                  color="success"
+                  sx={{
+                    textTransform: "capitalize",
+                  }}
+                />
+              ) : (
+                <Chip
+                  label={status}
+                  color="warning"
+                  sx={{
+                    textTransform: "capitalize",
+                  }}
+                />
+              )}
+
+              <div className="text-gray-900 text-[24px] sm:text-[32px] md:text-[40px] w-full md:w-[50%] font-bold">
+                {title}
+              </div>
             </div>
-          </div>
-          <div className="relative  border-green-200 pb-6 sm:pb-8 md:pb-10">
-            <div className="mt-6 sm:mt-8 md:mt-10 grid gap-4 sm:gap-6 md:gap-8">
-              <div>
-                {status === "approved" ? (
+            <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-1 sm:gap-2 md:gap-3">
+              Date and Time
+              <p className="text-gray-600 text-[12px] sm:text-[14px] font-medium">
+                {`From ${formatDate1(start)} to ${formatDate1(end)}`}
+              </p>
+            </div>
+            <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-2 sm:gap-3">
+              About this Event
+              <div className="w-full text-[12px] sm:text-[14px] text-gray-600 font-normal">
+                {description}
+              </div>
+            </div>
+            <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-2 sm:gap-3">
+              Tags
+              <div className="flex items-center justify-start gap-2 flex-wrap">
+                {tagArr.map((el) => (
                   <Chip
-                    label={status}
+                    label={el}
                     color="success"
                     sx={{
                       textTransform: "capitalize",
                     }}
                   />
-                ) : (
-                  <Chip
-                    label={status}
-                    color="warning"
-                    sx={{
-                      textTransform: "capitalize",
-                    }}
-                  />
-                )}
-
-                <div className="text-gray-900 text-[24px] sm:text-[32px] md:text-[40px] w-full md:w-[50%] font-bold">
-                  {title}
-                </div>
-              </div>
-              <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-1 sm:gap-2 md:gap-3">
-                Date and Time
-                <p className="text-gray-600 text-[12px] sm:text-[14px] font-medium">
-                  {`From ${formatDate1(start)} to ${formatDate1(end)}`}
-                </p>
-              </div>
-              <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-2 sm:gap-3">
-                About this Event
-                <div className="w-full text-[12px] sm:text-[14px] text-gray-600 font-normal">
-                  {description}
-                </div>
-              </div>
-              <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-2 sm:gap-3">
-                Tags
-                <div className="flex items-center justify-start gap-2 flex-wrap">
-                  {tagArr.map((el) => (
-                    <Chip
-                      label={el}
-                      color="success"
-                      sx={{
-                        textTransform: "capitalize",
-                      }}
-                    />
-                  ))}
-                </div>
-              </div>
-              <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-2 sm:gap-3">
-                Organized by
-                <p className="text-gray-600 text-[12px] sm:text-[14px] font-medium">
-                  {organizer ? organizer : "N/A"}
-                </p>
+                ))}
               </div>
             </div>
-            <div>
-              <hr
-                style={{
-                  height: "1px",
-                  backgroundColor: "#2E7D31",
-                  border: "none",
-                  marginTop: "1rem",
-                  marginBottom: "1rem",
-                }}
-              />
-            </div>
-            <div className="flex items-center justify-end gap-10">
-              {invoiceStatus === "Received" ? (
-                <Button
-                  variant="contained"
-                  color="success"
-                  className="absolute bottom-0 right-0"
-                  onClick={() => handeEActionvent("approve")}
-                  disabled={loadingApproval}
-                >
-                  Approve
-                </Button>
-              ) : (
-                <Button
-                  variant="contained"
-                  color="warning"
-                  className="absolute bottom-0 right-0"
-                  onClick={() => handeEActionvent("generateInvoice")}
-                >
-                  Make payment
-                </Button>
-              )}
-
-              <Button
-                variant="contained"
-                color="error"
-                className="absolute bottom-0 right-0"
-                onClick={() => handeEActionvent("decline")}
-                disabled={loadingDecline}
-              >
-                Decline
-              </Button>
+            <div className="text-[18px] sm:text-[20px] md:text-[22px] font-bold grid gap-2 sm:gap-3">
+              Organized by
+              <p className="text-gray-600 text-[12px] sm:text-[14px] font-medium">
+                {organizer ? organizer : "N/A"}
+              </p>
             </div>
           </div>
+          <div>
+            <hr
+              style={{
+                height: "1px",
+                backgroundColor: "#2E7D31",
+                border: "none",
+                marginTop: "1rem",
+                marginBottom: "1rem",
+              }}
+            />
+          </div>
+          <div className="flex items-center justify-end gap-10">
+            {invoiceStatus === "Received" ? (
+              <Button
+                variant="contained"
+                color="success"
+                className="absolute bottom-0 right-0"
+                onClick={() => handelActionEvent("approve")}
+                disabled={loadingApproval}
+              >
+                Approve
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="warning"
+                className="absolute bottom-0 right-0"
+                onClick={() => handelActionEvent("generateInvoice")}
+              >
+                Make payment
+              </Button>
+            )}
+
+            <Button
+              variant="contained"
+              color="error"
+              className="absolute bottom-0 right-0"
+              onClick={() => handelActionEvent("decline")}
+              disabled={loadingDecline}
+            >
+              Decline
+            </Button>
+          </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
 };
 
 export default EventDetails;
