f3a7d9de6ebcbfb18a75721d513e2633d307a178
Meedo365 <nuhuahmed365@gmail.com>
2024-08-18 19:21:56 +0100

    Update Auth.js

--- server/routes/endpoints/Auth.js
+++ server/routes/endpoints/Auth.js
@@ -27,12 +27,12 @@ const {
 let routes = (app) => {
   app.post("/register", createUser);
   app.post("/org/register", createOrganisationAsUser);
-  app.get("/users", getAllUsers);
+  app.get("/users", authAdmin, getAllUsers);
   app.get("/user/:id", getUserById);
   app.get("/token", auth, getUserByToken);
   app.put("/user/:id", updateUserById);
-  app.put("/add-delegate/:id", addDelegatesToOrganisation);
-  app.put("/password/user/", changeUserPasswordById);
+  app.put("/add-delegate/:id",auth, addDelegatesToOrganisation);
+  app.put("/password/user/",auth, changeUserPasswordById);
   app.delete("/user/:id", deleteUserById);
   app.post("/login", login);
   app.post("/logout", auth, logout);
