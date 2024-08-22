629c4c1623304c5b884b6f3d5ac80b694a1b1465
Meedo365 <nuhuahmed365@gmail.com>
2024-08-22 17:07:41 +0100

    Update users.js

--- server/models/users.js
+++ server/models/users.js
@@ -33,8 +33,8 @@ const userSchema = mongoose.Schema(
     },
     userType: {
       type: String,
-      enum: ["delegate", "organization"],
-      default: "delegate",
+      enum: ["organization"],
+      default: "organization",
     },
     organizationType: {
       type: String,
@@ -47,6 +47,10 @@ const userSchema = mongoose.Schema(
             required: true,
           },
           email: {
+            type: String,
+            required: true,
+          },
+          phone: {
             type: String,
             // required: true,
           },
