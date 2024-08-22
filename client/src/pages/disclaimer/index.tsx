0fab3db96733695be7566c9ffd07149966f78415
Chisomchima <chimachisom360@gmail.com>
2024-08-22 10:34:38 +0100

    terms and conditions update

--- client/src/pages/disclaimer/index.tsx
+++ client/src/pages/disclaimer/index.tsx
@@ -26,23 +26,89 @@ const TermsAndConditions = () => {
           color: "red",
         }}
       >
-        {/* THE DEADLINE FOR COP29 REGISTRATION  CLOSES ON 25th October 2024. */}
-        REGISTRATION FOR COP29 ON THE PORTAL WILL CLOSE ON 25th OCTOBER 2024.
+        Accreditation for UNFCCC COP29 will close on October 25, 2024.
       </p>
+
+      <section style={{ marginBottom: "20px" }}>
+        <h2 style={{ color: "#003300", fontSize: "20px" }}>
+          Presidential Directive
+        </h2>
+        <p style={{ lineHeight: "1.6" }}>
+          In accordance with the directive issued by President Bola Ahmed Tinubu
+          on August 21, 2024, to establish a Climate Accountability and
+          Transparency Platform and other measures to ensure efficiency and
+          accountability in the nation's participation in the 29th UN Climate
+          Change Conference (COP 29) in Baku, Azerbaijan, all prospective
+          members of the Federal government’s UNFCCC COP29 delegation are
+          hereby notified that accreditation requests are subject to
+          Presidential approval.
+        </p>
+        <p style={{ lineHeight: "1.6" }}>
+          Consequently, approvals will be granted only to individuals or
+          organizations that can clearly demonstrate that their participation
+          aligns with the theme for Nigeria, "Actualizing Financial Commitments
+          for Climate Action." Furthermore, the participation must have an
+          economic imperative and be directed toward engaging with companies,
+          multilateral partners, and stakeholders who will attract finance and
+          opportunities to Nigeria.
+        </p>
+      </section>
+
+      <section style={{ marginBottom: "20px" }}>
+        <h2 style={{ color: "#003300", fontSize: "20px" }}>
+          Required Documentation
+        </h2>
+        <p style={{ lineHeight: "1.6" }}>
+          All prospective delegation members are hereby required to upload
+          relevant supporting documents such as invitation letters or
+          correspondence to confirm:
+        </p>
+        <ul style={{ lineHeight: "1.6" }}>
+          <li>Participation is directly linked to high-level conversations to be held at COP 29, such as ongoing negotiations.</li>
+          <li>Signing of bilateral agreements and MoUs to concretize financial commitments.</li>
+          <li>Participation in thematic meetings as a representative of Nigeria.</li>
+        </ul>
+      </section>
+
+      <section style={{ marginBottom: "20px" }}>
+        <h2 style={{ color: "#003300", fontSize: "20px" }}>
+          Additional Guidelines
+        </h2>
+        <ul style={{ lineHeight: "1.6" }}>
+          <li>
+            Approvals will be granted strictly on merit, and full-and-real-time
+            access into the data concerning who is attending and those the
+            government is sponsoring to COP 29 will also be made available to
+            the general public on the Climate Accountability and Transparency
+            Portal (CAT-P) to ensure transparency.
+          </li>
+          <li>
+            The Nigeria delegation is to efficiently utilize the on-site
+            delegation office within the conference complex to host high-level
+            meetings and bilaterals. Members of the delegation will therefore be
+            required to schedule meetings ahead with the time-slotting system
+            provided on the portal.
+          </li>
+        </ul>
+      </section>
 
       <section style={{ marginBottom: "20px" }}>
         <h2 style={{ color: "#003300", fontSize: "20px" }}>Disclaimer</h2>
         <ul style={{ lineHeight: "1.6" }}>
           <li>
-            <strong>Governmental Priority:</strong> Only governmental
+            <strong>Governmental Priority:</strong> Only government
             organizations are automatically eligible for registration. Other
-            bodies may qualify and be considered if they apply for a side event
-            slot.
+            bodies such as CSOs, NGOs may qualify if their reason for
+            participation is aligned with the theme for Nigeria, "Actualizing
+            Financial Commitments for Climate Action," and the objective of
+            their participation has an economic imperative and is directed
+            toward engaging with companies, multilateral partners, and
+            stakeholders who will attract finance and opportunities to Nigeria.
           </li>
           <li>
-            <strong>Organizational Representation:</strong> Registration must be
-            completed through a recognized organization; individual applications
-            are not permitted.
+            <strong>Organizational Representation:</strong> Registration must
+            be completed through a recognized organization; individual
+            applications are not permitted.
           </li>
           <li>
             <strong>Mandate Alignment:</strong> The organization must have a
@@ -51,18 +117,15 @@ const TermsAndConditions = () => {
             policies, the UNFCCC's goals, and the specific objectives of COP29.
           </li>
           <li>
-            <strong>Limited Delegation:</strong>{" "}
-            <span className="text-red-500">
-              In compliance with Federal Government directives, organizations
-              may submit a maximum of two participants for accreditation. Up to
-              two additional participants may be considered for organizations
-              hosting side events.
-            </span>
+            <strong>Limited Delegation:</strong> In compliance with Federal
+            Government directives, organizations may submit a maximum of two
+            participants for accreditation. Up to two additional participants
+            may be considered for organizations hosting side events.
           </li>
           <li>
             <strong>Financial Responsibility:</strong> Participating
             organizations are responsible for covering all expenses related to
-            their representatives’ participation in COP29.
+            their representatives’ participation in the UNFCCC COP29.
           </li>
           <li>
             <strong>Fee for Side Events:</strong> Hosting a side event at COP29
@@ -112,8 +175,7 @@ const TermsAndConditions = () => {
             <strong>Post-COP Report:</strong> After COP29, a comprehensive
             report detailing sessions, events, and outcomes, endorsed by the
             organization's Head, must be submitted to the National Council on
-            Climate Change Secretariat at{" "}
-            <a href="mailto:info@natcccc.gov.ng">info@natcccc.gov.ng</a>.
+            Climate Change.
           </li>
         </ul>
       </section>
