72cf89c7875c829a9921cbb07c7b7518c5bc801e
Meedo365 <nuhuahmed365@gmail.com>
2024-08-29 22:07:41 +0100

    Update Message.js

--- server/routes/endpoints/Message.js
+++ server/routes/endpoints/Message.js
@@ -1,10 +1,13 @@
 const {
   sendInternationalMessage,
   sendContactMessage,
+  getAllMessages,
 } = require("../../controllers/message-controller");
+const {  authAdmin,  } = require("../../middlewares/middleware");
 let routes = (app) => {
   app.post("/international", sendInternationalMessage);
   app.post("/contact", sendContactMessage);
+  app.get("/meeting", authAdmin, getAllMessages);
 };
 
 module.exports = routes;
