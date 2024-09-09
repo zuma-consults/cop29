df3c6e7a44874ee0d01c23219e4a7d19ae03e0d9
Meedo365 <nuhuahmed365@gmail.com>
2024-09-09 23:35:40 +0100

    Update sendMail.js

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 
 const { SENDER_EMAIL_ADDRESS, SENDER_EMAIL_ADDRESS_FROM } = process.env;
 
-const sendEmail = async (to, name, amount) => {
+const sendEmail = async (to, name, code) => {
   const smtpTransport = nodemailer.createTransport({
     service: "gmail",
     port: 465,
@@ -23,105 +23,89 @@ const sendEmail = async (to, name, amount) => {
   const mailOptions = {
     from: SENDER_EMAIL_ADDRESS_FROM,
     to: to,
-    subject: "COP 29 INVOICE",
+    subject: "COP 29 DELEGATE STATUS",
     html: `
-    <div style="background-color: #f6f6f6; margin: 0; padding: 0;">
-  <div
-    style="
-      max-width: 650px;
-      margin: 0 auto;
-      background-color: #ffffff;
-      border-radius: 8px;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    "
-  >
+    <div style="background-color: #f6f6f6; margin: 0; padding: 0">
     <div
       style="
-        text-align: center;
-        padding: 20px 0;
-        background-color: #004d00;
-        color: #ffffff;
+        max-width: 650px;
+        margin: 0 auto;
+        background-color: #ffffff;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
       "
     >
-      <h5>Invoice - Cop29 Nigeria</h5>
-    </div>
-    <div style="padding: 50px; display: grid;">
-      <h3 style="color: #003300;">Hello ${name},</h3>
-      <p
+      <div
         style="
-          color: #336633;
-          font-size: 15px;
-          margin-bottom: 20px;
+          text-align: center;
+          padding: 20px 0;
+          background-color: #004d00;
+          color: #ffffff;
         "
       >
-        Your application has been processed, and you are required to pay the
-        amount of <span style="font-weight: bold;">${amount}</span> to reserve
-        your slot.
-      </p>
+        <h3>COP 29 Delegate</h3>
+      </div>
+      <div style="padding: 50px; display: grid">
+        <h3 style="color: #003300">Hello ${name},</h3>
+        <p style="color: #336633; font-size: 15px; margin-bottom: 20px">
+          We are pleased to inform you that your application has been
+          successfully processed and approved. Please find your unique QR code
+          as an attachment, which you are required to keep securely. This QR code will be
+          essential for marking your attendance at COP 29.
+        </p>
 
-      <div style="margin-bottom: 20px;">
-        <p style="margin: 5px 0;">
-          <span style="font-weight: bold;">Please use the following information for payment:</span>
+        <p style="color: #336633; font-size: 15px; margin-bottom: 20px">
+          Thank you for your participation, and we look forward to welcoming
+          you to the event.
         </p>
-        <p style="margin: 5px 0;">
-          Account Name: <span style="font-weight: bold;">[Account Name]</span>
-        </p>
-        <p style="margin: 5px 0;">
-          Account Number: <span style="font-weight: bold;">[Account Number]</span>
-        </p>
-        <p style="margin: 5px 0;">
-          Bank Name: <span style="font-weight: bold;">[Bank Name]</span>
+
+        <p style="color: #336633; font-size: 15px">
+          Warm regards,,<br />
+          COP29 Nigeria
         </p>
       </div>
-
-      <p
+      <div
         style="
-          color: #336633;
-          font-size: 15px;
-          margin-bottom: 20px;
+          text-align: center;
+          padding: 20px 70px;
+          background-color: #004d00;
+          color: #ffffff;
+          font-size: 14px;
         "
       >
-        Make sure to upload the payment evidence to confirm your reservation.
-      </p>
-
-      <p style="color: #336633; font-size: 15px;">
-        Thank you,<br />
-        Finance Officer, Cop29 Nigeria
-      </p>
-    </div>
-    <div
-      style="
-        text-align: center;
-        padding: 20px 70px;
-        background-color: #004d00;
-        color: #ffffff;
-        font-size: 14px;
-      "
-    >
-      <p style="color: whitesmoke; font-size: 11px; margin: 15px 0;">
-       Nigeris COP29 Theme: Actualizing financial commitments for Climate Action.
-      </p>
-      <p
-        style="
-          color: whitesmoke;
-          font-size: 11px;
-          display: flex;
-          justify-content: flex-start;
-          margin-top: 15px;
-        "
-      >
-        <a
-          href="https://cop29.vercel.app/"
-          style="color: whitesmoke; text-decoration: none;"
-          >Visit Website</a
+        <p style="color: whitesmoke; font-size: 11px; margin: 15px 0">
+          Nigeria COP29 Theme: Actualizing financial commitments for Climate
+          Action.
+        </p>
+        <p
+          style="
+            color: whitesmoke;
+            font-size: 11px;
+            display: flex;
+            justify-content: flex-start;
+            margin-top: 15px;
+          "
         >
-      </p>
-      <p style="color: whitesmoke; font-size: 11px;">© 2024 COP29 NIGERIA PORTAL</p>
+          <a
+            href="https://nigccdelegation.natccc.gov.ng/"
+            style="color: whitesmoke; text-decoration: none"
+            >Visit Website</a
+          >
+        </p>
+        <p style="color: whitesmoke; font-size: 11px">
+          © 2024 COP29 NIGERIA PORTAL
+        </p>
+      </div>
     </div>
   </div>
-</div>
-
     `,
+    attachments: [
+      {
+        filename: `${name}-code.png`,
+        path: code, // The QR code URL that was generated
+        cid: "qrCodeImage", // A unique identifier for embedding inline images if needed
+      },
+    ],
   };
 
   smtpTransport.sendMail(mailOptions, (err, info) => {
