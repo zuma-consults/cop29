02e2c9a9ca16fc090db3bf666d14c9cf2b645f6b
Meedo365 <nuhuahmed365@gmail.com>
2024-09-16 10:13:58 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,11 +9,34 @@ const rateLimit = require("express-rate-limit");
 
 const port = process.env.PORT || 7070;
 const { logRequestDuration } = require("./middlewares/middleware");
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // limit each IP to 100 requests per windowMs
+// });
 const helmet = require("helmet");
+const { errorHandler,systemError } = require("./utils/errorHandler");
+
+// const corsOptions = {
+//   // origin: function (origin, callback) {
+//   //   const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+//   //   if (allowedOrigins.indexOf(origin) !== -1 ) {
+//   //     callback(null, true);
+//   //   } else {
+//   //     callback(new Error('Not allowed by CORS'));
+//   //   }
+//   // },
+//   // methods: ['GET','POST'], // Only allow GET and POST requests
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Only allow specific headers
+//   exposedHeaders: ['Content-Length', 'X-Response-Time'], // Headers you want the client to see
+//   credentials: true, // Allow credentials (cookies, authorization headers)
+//   optionsSuccessStatus: 200, // Response status for preflight requests
+//   maxAge: 86400, // Cache preflight response for 24 hours
+// };
+
+app.options('*', cors(corsOptions), (req, res) => {
+  console.log('Preflight request:', req.headers);
+  res.sendStatus(200);
+});
 
 app.use(helmet());
 // app.use(
@@ -40,10 +63,10 @@ app.use(helmet());
 app.use(limiter);
 app.disable("x-powered-by");
 // Middleware setup
-app.use(cors()); // Enable CORS for all routes
+app.use(cors(corsOptions)); // Enable CORS for all routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logRequestDuration);
+// app.use(logRequestDuration);
 
 // Define API routes
 app.use("/api/v1", routes);
