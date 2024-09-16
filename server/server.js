68b66a571c6083250848807cba2c63080f6b5f6d
Meedo365 <nuhuahmed365@gmail.com>
2024-09-16 18:53:21 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,29 +14,30 @@ const limiter = rateLimit({
   max: 100, // limit each IP to 100 requests per windowMs
 });
 const helmet = require("helmet");
-const { errorHandler,systemError } = require("./utils/errorHandler");
+// const { errorHandler, systemError } = require("./utils/errorHandler");
 
-// const corsOptions = {
-//   // origin: function (origin, callback) {
-//   //   const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
-//   //   if (allowedOrigins.indexOf(origin) !== -1 ) {
-//   //     callback(null, true);
-//   //   } else {
-//   //     callback(new Error('Not allowed by CORS'));
-//   //   }
-//   // },
-//   // methods: ['GET','POST'], // Only allow GET and POST requests
-//   allowedHeaders: ['Content-Type', 'Authorization'], // Only allow specific headers
-//   exposedHeaders: ['Content-Length', 'X-Response-Time'], // Headers you want the client to see
-//   credentials: true, // Allow credentials (cookies, authorization headers)
-//   optionsSuccessStatus: 200, // Response status for preflight requests
-//   maxAge: 86400, // Cache preflight response for 24 hours
-// };
-
-// app.options('*', cors(corsOptions), (req, res) => {
-//   console.log('Preflight request:', req.headers);
-//   res.sendStatus(200);
-// });
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://admin-cop29.vercel.app",
+      "https://cop29-okike.vercel.app",
+      "https://nigccdelegation.natccc.gov.ng",
+    ];
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  // methods: ['GET','POST'], // Only allow GET and POST requests
+  // allowedHeaders: ['Content-Type', 'Authorization'], // Only allow specific headers
+  // exposedHeaders: ['Content-Length', 'X-Response-Time'], // Headers you want the client to see
+  // credentials: true, // Allow credentials (cookies, authorization headers)
+  // optionsSuccessStatus: 200, // Response status for preflight requests
+  // maxAge: 86400, // Cache preflight response for 24 hours
+};
 
 app.use(helmet());
 // app.use(
@@ -49,25 +50,14 @@ app.use(helmet());
 //   })
 // );
 
-// app.use(
-//   session({
-//     secret: 'your-secret',
-//     cookie: {
-//       secure: true, // Requires HTTPS
-//       httpOnly: true,
-//       sameSite: 'strict', // or 'lax'
-//     },
-//   })
-// );
-
 app.use(limiter);
 app.disable("x-powered-by");
 // Middleware setup
-// app.use(cors(corsOptions)); // Enable CORS for all routes
-app.use(cors()); // Enable CORS for all routes
+app.use(cors(corsOptions)); // Enable CORS for all routes
+// app.use(cors()); // Enable CORS for all routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(logRequestDuration);
+app.use(logRequestDuration);
 
 // Define API routes
 app.use("/api/v1", routes);
