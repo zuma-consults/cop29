eb3eeb67008644ceafc42e6ebef39e50b9c2160b
Meedo365 <nuhuahmed365@gmail.com>
2024-10-26 11:41:04 +0100

    Update index.tsx

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -64,7 +64,7 @@ const Profile: React.FC = () => {
   const { mutate: logout } = useLogout();
 
   useEffect(() => {
-    if (accreditationType === null || allow === null) {
+    if ( allow === null) {
       cookies.remove("accessToken");
       cookies.remove("profile");
       localStorage.clear();
